c072ed182b0bc319e3f9eb43f9ba5631542bec07
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-02-11 19:27:57 +0000

    Project created

---
+++ src/test/java/com/example/springBoot1/SpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBoot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
