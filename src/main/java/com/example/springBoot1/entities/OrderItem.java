03851fc7f150c12d85a4be7246d7ba276b0011bb
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-02-15 08:07:22 +0000

    Product-OrderItem oneToMany association

--- src/main/java/com/example/springBoot1/entities/OrderItem.java
+++ src/main/java/com/example/springBoot1/entities/OrderItem.java
@@ -33,7 +33,7 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 	
-	@JsonIgnore
+	@JsonIgnore // to eliminate the infinite loop
 	public Order getOrder() {
 		return id.getOrder();
 	}
